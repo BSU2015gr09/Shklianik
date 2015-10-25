e39d754a8f85a549914b36ac5633b72e11f6e16b
VShklianik <lera_shklianik@mail.ru>
2015-10-25 21:04:05 +0400

    dz3

---
+++ random_dz3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>//рандом
+#include <clocale>
+
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int mas[20];
+	int a = 4096, c = 150889, m = 714025;
+	mas[0] = 1;
+	cout << mas[0] << "=:1" << endl;
+	for (int i = 1; i<20; i++) {
+		mas[i] = (a*mas[i - 1] + c) % m;
+		cout << mas[i] << "=:" << i + 1 << endl;
+	}
+	return 0;
+}
