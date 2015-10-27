84626ed5e8d126a545e44562e50f4049c8c9a982
barvenov <barvenovsa@gmail.com>
2015-10-27 11:26:05 +0400

    Замечания по коду...

--- random_dz3.cpp
+++ random_dz3.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	int mas[20]; // зачем массив нужен???
+	int mas[20]; // зачем массив нужен??? 
 	int a = 4096, c = 150889, m = 714025;
 	mas[0] = 1;
 	cout << mas[0] << "=:1" << endl;
-	for (int i = 1; i<20; i++) {
+	for (int i = 1; i<20; i++) { // Почему только 20???
 		mas[i] = (a*mas[i - 1] + c) % m;
 		cout << mas[i] << "=:" << i + 1 << endl;
 	}
